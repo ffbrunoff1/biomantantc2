bf28886be67a5c298fa67afdacc9ea29bcc1dabd
ffbrunoff1 <bruno@ntcbrasil.com.br>
2025-08-08 11:52:51 -0300

    Atualizar arquivos do projeto - 2025-08-08T14:52:51.436Z

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,6 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
-    minify: 'terser',
     rollupOptions: {
       output: {
         manualChunks: {
